a5a07dd43c3339d5afbcdfd0670869a82eee0ba7
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2022-11-28 21:38:03 +0100

    update
    New exercises.

--- practice.py
+++ practice.py
@@ -183,13 +183,308 @@ def increment_string(strng):
 
 increment_string("foobar00")
 #______________________________________
+import math
+from statistics import mean
+def solution(array_a, array_b):
+    lst,sum = [],0
+    for i in range(len(array_a)):
+        i = abs(i)
+        if array_a[i] == array_b[i]:
+            lst.append(0)
+        else:
+            lst.append(abs((array_a[i])-(array_b[i]))**2)
+            print(lst)
+    print(mean(lst))
+    return mean(lst)
+
+
+a1 = [1,2,3]
+a2 = [4,5,6]
+solution(a1, a2)
+#______________________________________
+def find_even_index(arr):
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    return -1
+    
+find_even_index([20,10,30,10,10,15,35])
+#______________________________________
+import random
+def guess():
+    a = 5
+    b = int(input("Enter a number: "))
+    while a != b:
+        if b < a:
+            print("too low")
+            b = int(input("try again: "))
+        elif b > a:
+            print("too high")
+            b = int(input("try again: "))
+        else:
+            break
+    print("you won")
+    replay = input("Do you want to play again: (yes,Yes,y/no,No,n): ")
+    if replay == "yes" or replay == "Yes" or replay == "y":
+        result == 0
+    else:
+        print("bye bye")
+        result = result+1
+          
+guess()
+#______________________________________
+def rect_with_hole(rows,cols,symb):
+    lst=[]
+    belly = []
+    lst.append(f"{symb}"*rows)
+    lst.append(" "*rows)
+    belly = symb+lst[1][1:-1]+symb
+   
+    for row in range(1,2):
+        #print("# "*cols)
+        for col in range(1,cols+1):
+            if col == row or col == cols:
+                print(lst[0])
+            else:
+                print(belly)
+        
+rect_with_hole(8, 3,"#") 
+#______________________________________
+def pira(n):
+    n=n+1
+    for y in range(n,0,-1):
+        print(" "*(y-1)+"# "*(n-y)) 
+
+pira(7)
+#______________________________________
+def find_t(lst_of_t):
+    bigger = [0,'']
+    for i in lst_of_t:
+        if bigger[0] < (i[1]*i[2])/2:
+            bigger[0] = (i[1]*i[2])/2
+            bigger[1] = i[0]
+    print(bigger[1])
+    return bigger[1]
+            
+rectangles = [('red', 23400, 12484), ('green', 1567, 873), ('blue', 928, 876)]   
+find_t(rectangles)
+#______________________________________
+def bigger(num):
+    lst = num.split(' ')
+    if int(lst[0])>int(lst[1]):
+        print(lst[0])
+        return lst[0]
+    print(lst[1])
+    return lst[1]
+
+bigger('177 32') 
+#______________________________________
+def snail(array):
+    results = []
+    while len(array) > 0:
+        # go right
+        results += array[0]
+        del array[0]
+
+        if len(array) > 0:
+            # go down
+            for i in array:
+                results += [i[-1]]
+                del i[-1]
+
+            # go left
+            if array[-1]:
+                results += array[-1][::-1]
+                del array[-1]
+
+            # go top
+            for i in reversed(array):
+                results += [i[0]]
+                del i[0]
+
+    return results
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array)
+#______________________________________
+def rot13(message):
+    encrypt = ''
+    for i in list(message):
+        if 123>ord(i)>96:
+            if ord(i)+13 > 122:
+                encrypt = encrypt + chr((ord(i)+13) - 26)
+            else:
+                encrypt = encrypt + chr((ord(i)+13))
+        elif 91>ord(i)>64:
+            if ord(i)+13 > 90:
+                encrypt = encrypt + chr((ord(i)+13) - 26) 
+            else:
+                encrypt = encrypt + chr((ord(i)+13))
+        else:
+            encrypt = encrypt + i
+    print(encrypt)        
+    return encrypt
+        
+rot13('aA bB zZ 1234 *!?%')
+#______________________________________
+def zeros(n):
+    fac = 1
+    counter = 0
+    biggest = 0
+    while n != 0:
+        fac = n * fac
+        n-=1
+
+    for i in str(fac):
+        if int(i) == 0:
+            counter +=1
+        else:
+            if counter >= biggest:
+                biggest = counter
+                counter = 0
+            else:
+                counter = 0 
+
+    if counter > biggest:
+        biggest = counter
+        counter = 0
+
+    print (biggest)
+    return biggest
+
+zeros(1000)
+#______________________________________
+def make_readable(seconds):
+    if seconds > 359999 or seconds < 0:
+        return False
+    else:
+        hours = seconds // 3600 % 24
+        minutes = seconds // 60 % 60
+        seconds = seconds % 60
+        time = [str(hours), str(minutes), str(seconds)]
+
+        for i in time:
+            if iftwodigit(i) == 1:
+                time[time.index(i)] = '0' + time[time.index(i)]
+
+        return f'{time[0]}:{time[1]}:{time[2]}'
+
+ 
+def iftwodigit(num):
+    print (len(str(num)))
+    return len(str(num))
+
+make_readable(3542)
+#______________________________________
+def valid_parentheses(string):
+    lst_ = list(string)
+    lst_temp = []
+    if ('(' in lst_ and ')' in lst_) or string == '':
+        if string =='':
+            return True
+        elif lst_.index(')') < lst_.index('('):
+            print('false!!!!')
+            return False
+
+        for item in string:
+            try:
+                if item == '(':
+                    lst_temp.append(item)
+            
+                elif item ==')':
+                    lst_temp.pop()
+            except IndexError:
+                return False
+        if '(' not in lst_temp:
+            return True
+        else:
+            return False
+    else:
+        return False           
+
+valid_parentheses("s(h()r)k(")
+#______________________________________
+def rgb(r, g, b):
+    rgb_lst = [r,g,b]
+    hex_ = ''
+    ecumulator = 0
+    for num in rgb_lst:
+        if num > 255:
+            rgb_lst[rgb_lst.index(num)] = 255
+        elif num < 0:
+            rgb_lst[rgb_lst.index(num)] = 0
+    
+
+
+    for num in rgb_lst:
+        num = num / 16
+        ecumulator = list(math.modf(num))[0] * 16
+
+        hex_ = hex_ + str(dectohex(int(num)))
+        hex_ = hex_ + str(dectohex(int(ecumulator)))
+    print(hex_)
+    return hex_
+
+rgb(255,-255,255)
+#______________________________________
+class Account:
+    def __init__(self,owner,balance):
+        self.owner = owner
+        self.bal = balance
+
+    def owner(self):
+        return self.owner
+
+    def balance(self):
+        return self.bal
+
+    def deposit(self, add_money):
+        print("Deposit Accepted")
+        self.bal = self.bal + add_money
+
+    def withdraw(self, sub_money):
+        withdraw_temp = self.bal - sub_money
+        if not withdraw_temp < 0:
+            self.bal = withdraw_temp
+            print("Withdrawal Accepted")
+            return withdraw_temp
+        else:
+            print("Funds Unavailable!")
+    
+    def __str__(self):
+        return "Account owner:{}\nAccount balance:{} ".format(self.owner,self.bal)
+
+acct1 = Account('Jose',100)
+print(acct1.withdraw(101))
+#______________________________________
+class Line:
+    
+    def __init__(self,coor1,coor2):
+        print("New coor is assianed")
+        self.x1= coor1[0]
+        self.x2= coor2[0]
+        self.y1= coor1[1]
+        self.y2= coor2[1]
+
+        self.coor1 = coor1
+        self.coor2 = coor2
+    
+    def distance(self):
+        return math.dist(self.coor1,self.coor2)
+    
+    def slope(self):
+        return (self.y2-self.y1)/(self.x2-self.x1)
+
+coordinate1 = (3,2)
+coordinate2 = (8,10)
+
+li = Line(coordinate1,coordinate2)
+print(li.distance())
+print(li.slope())
 #______________________________________
 #______________________________________
 #______________________________________
-#______________________________________
-#______________________________________
-#______________________________________
-#______________________________________
-#______________________________________
-#______________________________________
-#______________________________________
+
+
